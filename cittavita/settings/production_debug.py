1c5c1d0d213f1cd06fbe038c2987158621e16fbe
ViewFuhrer <atlantij@gmail.com>
2013-06-04 17:36:05 +0400

    cfgs

--- cittavita/settings/production_debug.py
+++ cittavita/settings/production_debug.py
@@ -1,5 +1,8 @@
 from production import *
 
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
 INSTALLED_APPS += ('debug_toolbar', )
 
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware', )
