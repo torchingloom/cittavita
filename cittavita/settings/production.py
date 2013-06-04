261c8023a4909c19f7f48d58b146e5d36194c84f
ViewFuhrer <atlantij@gmail.com>
2013-06-04 17:41:17 +0400

    rem redis in prod

--- cittavita/settings/production.py
+++ cittavita/settings/production.py
@@ -9,6 +9,6 @@ DATABASES['default']['USER'] = DATABASES['default']['NAME']
 DATABASES['default']['PASSWORD'] = 'b36b888c'
 
 SESSION_ENGINE = 'redis_sessions.session'
-INSTALLED_APPS += ('cacheops', )
+#INSTALLED_APPS += ('cacheops', )
 
 REDIS_CLI_PATH = '''/home/atlantij/lib/redis-2.6.13/src/redis-cli'''
